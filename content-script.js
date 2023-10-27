720582bb6a7d61e8ee918d7668684513d5161b63
tsmurer <tsmurer@Ubuntu-22.04.2.myguest.virtualbox.org>
2023-10-27 16:39:10 -0300

    add observer for dynamic content loading

--- content-script.js
+++ content-script.js
@@ -1,36 +1,46 @@
-console.log("starting content-scripts.js")
-changeRecommendationList(true)
-changeThumbnails(true)
+console.log("Starting content-scripts.js");
 
-try {
-    document.addEventListener('DOMContentLoaded', function () {
-        // Now that the page is fully loaded, handle messages from the popup
-        chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-            if (message.text === 'Hello from popup!') {
-                // Handle the message received from the popup
-                console.log('Message from popup:', message.text);
-    
-                // You can perform any action here in response to the message.
-            }
-        });
-    });
-} catch(e) {
-    console.log(e)
-}
-
-
-
+// Function to change the recommendation list
 function changeRecommendationList(shouldItGoAway) {
-    console.log("changeRecommendationList(" + shouldItGoAway + ")");
-    let rightColumn = document
-    console.log(rightColumn)
-    rightColumn.style.display = shouldItGoAway ? "none" : "block";
+    const rightColumn = document.querySelector("#related");
+    if (rightColumn) {
+        rightColumn.style.display = shouldItGoAway ? "none" : "block";
+    }
 }
 
+// Function to change the thumbnails
 function changeThumbnails(shouldThemGoAway) {
-    console.log("changeThumbnails(" + shouldThemGoAway + ")");
     const thumbnails = document.querySelectorAll('[id=thumbnail]');
     thumbnails.forEach((thmb) => {
-        thmb.style.display = shouldThemGoAway ? "none" : "block"; // Fix the typo here
+        thmb.style.display = shouldThemGoAway ? "none" : "block";
     });
+}
+
+// Create a MutationObserver to observe changes in the DOM
+const observer = new MutationObserver((mutationsList, observer) => {
+    mutationsList.forEach((mutation) => {
+        if (mutation.addedNodes && mutation.addedNodes.length > 0) {
+            changeRecommendationList(true);
+            changeThumbnails(true);
+        }
+    });
+});
+
+// Define the observer configuration
+const observerConfig = {
+    childList: true,
+    subtree: true,   
+};
+
+// Start observing the document
+observer.observe(document, observerConfig);
+
+// The following code listens for messages from a Chrome extension's popup
+try {
+    document.addEventListener('DOMContentLoaded', function () {
+        chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+        });
+    });
+} catch (e) {
+    console.log(e);
 }
